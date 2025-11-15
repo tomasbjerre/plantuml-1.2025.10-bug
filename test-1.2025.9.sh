975f6549eaf421e1b341652a2424a0301f886e42
Bjerre Tomas (66128244) <tomas.bjerre@forsakringskassan.se>
2025-11-15 09:57:19 +0100

    bug

---
+++ test-1.2025.9.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo Only d1:
+java -jar plantuml-1.2025.9.jar -overwrite -charset UTF-8 -I d1.plantuml
+ls
+rm *.png
+echo
+
+
+echo Only d2:
+java -jar plantuml-1.2025.9.jar -overwrite -charset UTF-8 -I d2.plantuml
+ls
+rm *.png
+echo
+
+echo All:
+java -jar plantuml-1.2025.9.jar -overwrite -charset UTF-8 -I *.plantuml
+ls
+rm *.png
+echo
+
+echo All twice:
+java -jar plantuml-1.2025.9.jar -overwrite -charset UTF-8 -I *.plantuml
+java -jar plantuml-1.2025.9.jar -overwrite -charset UTF-8 -I *.plantuml
+ls
+rm *.png
+echo
